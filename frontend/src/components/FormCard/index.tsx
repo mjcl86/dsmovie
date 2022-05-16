350abe1a02af2ae36107a0c76feffbf06f2c2495
mariojcl <mariojcl2020@gmail.com>
2022-05-16 16:36:37 +0100

    final

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,4 +1,3 @@
-import { data } from 'autoprefixer';
 import axios, { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
